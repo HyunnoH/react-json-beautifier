96ed6ca233581c0c9e423b8e439af6dd88d30eef
HyunnoH <hyunnoh01@gmail.com>
2020-08-05 19:38:20 +0000

    Initial setup

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+interface JSONBeautifierProps {
+  json: JSON | object;
+}
+
+export default function JSONBeautifier({ json }: JSONBeautifierProps) {
+  return <div></div>;
+}
